632b50fa84c5a82119a6088142a4b71b8ef9f565
adrieliket <adrielikethin.dossantos@gmail.com>
2022-08-25 22:44:10 -0300

    crud de estado, falta o controller de marca e categoria

---
+++ backend/src/main/java/com/dev/backend/entity/Categoria.java
@@ -0,0 +1,38 @@
+package com.dev.backend.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "categorias")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 4048798961366546485L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    @CreationTimestamp
+    private Timestamp dataCadastro;
+
+    @UpdateTimestamp
+    private Timestamp dataModificacao;
+
+}
