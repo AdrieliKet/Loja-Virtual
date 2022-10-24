b3ba9fdca4e003c03afc7ab46ab2e777d94ed21c
adrieliket <adrielikethin.dossantos@gmail.com>
2022-10-24 13:14:43 -0300

    Adicionando frontend e iniciando o crud de estado no frontend

--- backend/src/main/java/com/dev/backend/controller/EstadoController.java
+++ backend/src/main/java/com/dev/backend/controller/EstadoController.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.dev.backend.entity.Estado;
 import com.dev.backend.service.EstadoService;
@@ -25,21 +18,25 @@ public class EstadoController {
     private EstadoService estadoService;
 
     @GetMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public List<Estado> buscarTodos(){
         return estadoService.buscarTodos();
     }
 
     @PostMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Estado inserir(@RequestBody Estado estado){
         return estadoService.inserir(estado);
     }
 
     @PutMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Estado alterar(@RequestBody Estado estado) {
         return estadoService.alterar(estado);
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
         estadoService.excluir(id);
         return ResponseEntity.ok().build();
